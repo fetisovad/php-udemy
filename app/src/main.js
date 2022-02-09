3a4df7eb3ea69a4861dc876dd1260b42e6cdb050
Alexandr Fetisov <a.d.fetisov@tmn3.etagi.com>
2022-02-09 19:09:10 +0500

    end lesson 7

--- app/src/main.js
+++ app/src/main.js
@@ -1,34 +1,5 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import Editor from "./components/editor/editor";
+import Editor from "./components/editor";
 
-ReactDOM.render(<Editor/>, document.getElementById('root'))
-
-function getPageList() {
-    $('h2').remove();
-    $.get(
-        './api',
-        (data) => {
-            data.forEach((file) => {
-                $('body').append(`<h2>${file}</h2>`);
-            });
-        },
-        'JSON'
-    );
-}
-
-getPageList();
-
-$('button').click(() => {
-    $.post(
-        './api/createNewPage.php',
-        {
-            name: $('input').val(),
-        },
-        () => {
-            getPageList();
-        }
-    ).fail(() => {
-        alert('Страница уже существует!');
-    });
-});
+ReactDOM.render(<Editor/>, document.getElementById('root'));
